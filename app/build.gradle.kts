bef955fb3b591b5ccb0098d46ddd4afa6ccc7fdd
Siti Iedrania <68504848+iedrania@users.noreply.github.com>
2024-05-09 20:50:55 +0700

    feat: add tflite helper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        mlModelBinding = true
     }
 }
 
@@ -50,4 +51,7 @@ dependencies {
 
 
     // TODO: Tambahkan Library TensorFlow Lite
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 }
