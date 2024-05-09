cd096f8f3059583e43a337a2bc09cf2f037614a8
Siti Iedrania <68504848+iedrania@users.noreply.github.com>
2024-05-09 21:51:41 +0700

    feat: integrate helper to main

--- app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.dicoding.asclepius.view
 
-import android.Manifest
 import android.content.Intent
+import android.icu.text.NumberFormat
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +11,8 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityMainBinding
+import com.dicoding.asclepius.helper.ImageClassifierHelper
+import org.tensorflow.lite.task.vision.classifier.Classifications
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -56,12 +58,38 @@ class MainActivity : AppCompatActivity() {
 
     private fun analyzeImage() {
         // TODO: Menganalisa gambar yang berhasil ditampilkan.
-        moveToResult()
+        val imageClassifierHelper = ImageClassifierHelper(
+            context = this,
+            classifierListener = object : ImageClassifierHelper.ClassifierListener {
+                override fun onError(error: String) {
+                    runOnUiThread {
+                        showToast(error)
+                    }
+                }
+
+                override fun onResults(results: List<Classifications>?) {
+                    runOnUiThread {
+                        results?.let {
+                            if (it.isNotEmpty() && it[0].categories.isNotEmpty()) {
+                                println(it)
+                                val category = it[0].categories[0]
+                                val displayResult =
+                                    "${category.label} " + NumberFormat.getPercentInstance()
+                                        .format(category.score)
+                                moveToResult(displayResult)
+                            }
+                        }
+                    }
+                }
+            },
+        )
+        currentImageUri?.let { imageClassifierHelper.classifyStaticImage(it) }
     }
 
-    private fun moveToResult() {
+    private fun moveToResult(displayResult: String) {
         val intent = Intent(this, ResultActivity::class.java)
         intent.putExtra(ResultActivity.EXTRA_IMAGE_URI, currentImageUri.toString())
+        intent.putExtra(ResultActivity.EXTRA_RESULT, displayResult)
         startActivity(intent)
     }
 
