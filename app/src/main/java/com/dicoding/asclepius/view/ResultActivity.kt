c562236d1ac2e77dbd12840fb65993351e87f3dd
Siti Iedrania <68504848+iedrania@users.noreply.github.com>
2024-05-09 20:25:16 +0700

    feat: add working gallery

--- app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
@@ -1,8 +1,9 @@
 package com.dicoding.asclepius.view
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.asclepius.R
+import android.util.Log
 import com.dicoding.asclepius.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
@@ -10,10 +11,24 @@ class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // TODO: Menampilkan hasil gambar, prediksi, dan confidence score.
+        val imageUri = Uri.parse(intent.getStringExtra(EXTRA_IMAGE_URI))
+        imageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.resultImage.setImageURI(it)
+        }
+
+        val result = intent.getStringExtra(EXTRA_RESULT)
+        result?.let {
+            binding.resultText.text = it
+        }
     }
 
-
+    companion object {
+        const val EXTRA_IMAGE_URI = "extra_image_uri"
+        const val EXTRA_RESULT = "extra_result"
+    }
 }
